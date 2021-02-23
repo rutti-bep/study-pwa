285bf259e2e2d017d7341c7580689afddf0a3bc3
rutti-bep <s.konnno.train233@gmail.com>
2021-02-23 17:21:25 +0900

    wip

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 console.log("sw run")
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
-  navigator.clipboard.writeText("serviceworker").catch(e=>console.log(e));
-  navigator.clipboard.readText().then(
-  clipText => console.log(clipText));
 
 });
 self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Fetched resource '+e.request.url);
+  navigator.clipboard.writeText("serviceworker").catch(e=>console.log(e));
+  navigator.clipboard.readText().then(
+  clipText => console.log(clipText));
 });
