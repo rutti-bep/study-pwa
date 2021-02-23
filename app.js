d6d3c89c9e6f61026f92b6079c4d6d031ef7191a
rutti-bep <s.konnno.train233@gmail.com>
2021-02-23 17:19:14 +0900

    wip #9

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ console.log("run app")
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/study-pwa/sw.js');
 }
+
+navigator.clipboard.writeText("txt").then(e=>console.log(e));
+navigator.clipboard.readText().then(
+  clipText => console.log(clipText));
+
--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 console.log("sw run")
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+  console.log('[Service Worker] Install');
+  navigator.clipboard.writeText("txt").catch(e=>console.log(e));
+  navigator.clipboard.readText().then(
+  clipText => console.log(clipText));
+
 });
 self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Fetched resource '+e.request.url);
